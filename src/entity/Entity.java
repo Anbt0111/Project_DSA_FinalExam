c59a352c9d8362f4379ceb4e01b0a8c8de91cf66
Anbt011 <buithoan_t67@hus.edu.vn>
2023-12-15 23:34:05 +0700

    update entity

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -3,26 +3,5 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class Entity {
-    int health, attack, spriteNum, spriteTime = 1;
-    int state = 0; // 0 = idle, 1 = attack, 2 = hurt
-    final int SPRITE_INTERVAL = 3;
-    BufferedImage[] idleSprite, attackSprite, hurtSprite;
-    BufferedImage currentSprite;
-    final int MAX_SPRITE_NUMBER= 3;
-    public static final int IDLE = 0;
-    public static final int ATTACK = 1;
-    public static final int HURT = 2;
-
-    public Entity(int health) {
-        this.health = health;
-    }
-    public BufferedImage scaleImage(BufferedImage org, int width, int height) {
-        BufferedImage scaled = new BufferedImage(width, height, org.getType());
-        Graphics2D g2 = scaled.createGraphics();
-        g2.drawImage(org, 0, 0, width, height, null);
-        g2.dispose();
-
-        return scaled;
-    }
 }
 
