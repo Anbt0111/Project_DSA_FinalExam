16df971ff4ca1ca02f1863a3d87258201d1cc707
TruongQuocAnh76 <truongquocanh0706@gmail.com>
2023-12-17 11:46:52 +0700

    add main menu, game panel, help panel, high score panel, exit utility, end screen panel

---
+++ src/view/GamePanel.java
@@ -0,0 +1,363 @@
+package view;
+
+import entity.*;
+import java.awt.*;
+import javax.swing.*;
+import module.Countdown;
+import module.InGameSpriteManager;
+
+@SuppressWarnings("all")
+public class GamePanel extends JPanel implements Runnable {
+  private final int fps = 60;
+  public int score = 0;
+  JPanel symbolPanel = new JPanel();
+  private JButton removeButton;
+  private boolean isStartOfTheGame = true;
+  private View view;
+  private Gojo gojo = new Gojo();
+  private Dragon dragon = new Dragon();
+  private Image backgroundImage;
+  private Countdown timer = new Countdown();
+  public JLabel clockLabel =
+      new JLabel() {
+        @Override
+        public void paintComponent(Graphics g) {
+          super.paintComponent(g);
+          Graphics2D g2 = (Graphics2D) g;
+          g2.setFont(view.gameFont.deriveFont(Font.BOLD, 80));
+          g2.drawString(timer.getTime() + "", View.GRID_WIDTH / 3, View.GRID_HEIGHT * 3 / 2);
+        }
+      };
+  private InGameSpriteManager spriteManager = new InGameSpriteManager();
+  private boolean isMemorizePhase = true;
+  private boolean isCastingPhase = false;
+  private JLabel symbolTable = new JLabel();
+  private JPanel inputPanel = new JPanel();
+  private JLabel scoreLabel = new JLabel();
+  private Thread thread = new Thread(this);
+
+  public GamePanel(View view) {
+    this.view = view;
+    this.setLayout(null);
+
+    addSymbolTable();
+
+    this.backgroundImage =
+        new ImageIcon(getClass().getClassLoader().getResource("resource/images/background.png"))
+            .getImage();
+
+    ImageIcon clock =
+        new ImageIcon(getClass().getClassLoader().getResource("resource/images/clock.png"));
+    clock =
+        new ImageIcon(
+            clock
+                .getImage()
+                .getScaledInstance(View.GRID_WIDTH, View.GRID_HEIGHT * 2, Image.SCALE_SMOOTH));
+    clockLabel.setIcon(clock);
+    clockLabel.setBounds(0, View.GRID_HEIGHT * 13, View.GRID_WIDTH, View.GRID_HEIGHT * 2);
+    this.add(clockLabel);
+
+    JLabel exitButton = new JLabel();
+    exitButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+    exitButton.setName("back");
+    ImageIcon exitIcon =
+        new ImageIcon(getClass().getClassLoader().getResource("resource/images/exit_button.png"));
+    exitIcon =
+        new ImageIcon(
+            exitIcon
+                .getImage()
+                .getScaledInstance(View.GRID_WIDTH * 2, View.GRID_HEIGHT * 2, Image.SCALE_SMOOTH));
+    exitButton.setIcon(exitIcon);
+    exitButton.setBounds(
+        14 * View.GRID_WIDTH, 13 * View.GRID_HEIGHT, View.GRID_WIDTH * 2, View.GRID_HEIGHT * 2);
+    exitButton.addMouseListener(view.mouseController);
+    this.add(exitButton);
+
+    // Create and configure the remove button with the transparent image
+    removeButton = new JButton();
+    removeButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+    removeButton.setName("remove");
+
+    // Load the transparent image
+    ImageIcon removeIcon =
+        new ImageIcon(getClass().getClassLoader().getResource("resource/images/remove_button.png"));
+
+    // Ensure the image has a transparent background
+    removeIcon =
+        new ImageIcon(
+            removeIcon
+                .getImage()
+                .getScaledInstance(View.GRID_WIDTH * 2, View.GRID_HEIGHT * 2, Image.SCALE_SMOOTH));
+    // Set the button to be transparent
+    removeButton.setContentAreaFilled(false);
+    removeButton.setBorderPainted(false);
+
+    removeButton.setIcon(removeIcon);
+    removeButton.setBounds(
+        14 * View.GRID_WIDTH, 11 * View.GRID_HEIGHT, View.GRID_WIDTH * 2, View.GRID_HEIGHT * 2);
+    removeButton.addMouseListener(view.mouseController);
+    this.add(removeButton);
+
+    scoreLabel.setFont(view.gameFont.deriveFont(Font.BOLD, 40));
+    scoreLabel.setForeground(Color.BLACK);
+    scoreLabel.setBounds(14 * View.GRID_WIDTH, 0, View.GRID_WIDTH * 2, View.GRID_HEIGHT);
+    scoreLabel.setText("Score: " + score);
+    this.add(scoreLabel);
+  }
+
+  public void removeLastSymbol() {
+    if (!gojo.getSpells().isEmpty()) {
+      gojo.getSpells().pop(); // Remove the last symbol from the stack
+      // Remove the last added symbol from the symbolTable
+      int lastSymbolIndex = symbolTable.getComponentCount() - 1;
+      if (lastSymbolIndex >= 0) symbolTable.remove(lastSymbolIndex);
+    }
+  }
+
+  private void addSymbolTable() {
+    // table to receive player input
+    inputPanel.setOpaque(false);
+
+    JLabel inputTable = new JLabel();
+    inputTable.setLayout(null);
+    spriteManager.setTableSpriteSize(8 * View.GRID_WIDTH, 2 * View.GRID_HEIGHT);
+    inputTable.setIcon(spriteManager.getTableSprite());
+    inputPanel.setBounds(
+        4 * View.GRID_WIDTH, 13 * View.GRID_HEIGHT, 8 * View.GRID_WIDTH, 2 * View.GRID_HEIGHT);
+    inputPanel.add(inputTable);
+
+    spriteManager.setSymbolSpriteSize(View.GRID_WIDTH, View.GRID_HEIGHT);
+    for (int i = 1; i <= 6; i++) {
+      JLabel symbol = new JLabel();
+      symbol.addMouseListener(view.mouseController);
+      symbol.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+      symbol.setName("symbol" + i);
+      symbol.setIcon(spriteManager.getSymbolSprite(i));
+      symbol.setBounds(
+          View.GRID_WIDTH / 2 + (View.GRID_WIDTH * (i - 1) + View.GRID_WIDTH / 5 * (i - 1)),
+          View.GRID_HEIGHT / 2,
+          View.GRID_WIDTH,
+          View.GRID_HEIGHT);
+      inputTable.add(symbol);
+    }
+    this.add(inputPanel);
+
+    // table to display weakness
+    symbolTable.setLayout(null);
+
+    symbolPanel.setBackground(
+        new Color(
+            0, 0, 0, 0)); // for some reason, setOpaque(false) doesn't work, so i use this instead
+    symbolPanel.setBounds(
+        4 * View.GRID_WIDTH, 6 * View.GRID_HEIGHT, 7 * View.GRID_WIDTH, 2 * View.GRID_HEIGHT);
+    spriteManager.setTableSpriteSize(7 * View.GRID_WIDTH, 2 * View.GRID_HEIGHT);
+    symbolTable.setIcon(spriteManager.getTableSprite());
+    symbolPanel.add(symbolTable);
+    this.add(symbolPanel);
+  }
+
+  @Override
+  public void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    Graphics2D g2 = (Graphics2D) g;
+    g2.drawImage(backgroundImage, 0, 0, this.getWidth(), this.getHeight(), this);
+    gojo.draw(g2);
+    dragon.draw(g2);
+  }
+
+  private void attackPhase() {
+    if (!timer.isCounting()) { // end of attack phase, back to memorize phase
+      // stop counting
+      timer.stopCounting();
+      // transit to memorize phase
+      isMemorizePhase = true;
+      // change state of entities to idle
+      gojo.setState(Entity.IDLE);
+      dragon.setState(Entity.IDLE);
+
+      // check if game is over
+      if (gojo.getHealth() == 0) {
+        view.updateScore();
+        view.changePanel("endScreen");
+        stop();
+      }
+
+      memorizePhaseSetup();
+    }
+  }
+
+  private void memorizePhaseSetup() {
+    // show weakness table
+    symbolTable.setVisible(true);
+    symbolPanel.setVisible(true); // as the panel is the parent of the table, it must be visible too
+
+    inputPanel.setVisible(false); // hide input table
+
+    dragon.revealWeakness();
+    // adding weakness symbols to weakness table
+    symbolTable.removeAll(); // remove all previous symbols
+    spriteManager.setSymbolSpriteSize(View.GRID_WIDTH, View.GRID_HEIGHT);
+    for (int i = 0; i < 5; i++) {
+      JLabel symbol = new JLabel();
+      symbol.setIcon(spriteManager.getSymbolSprite(dragon.getWeakness().get(i).getIndex()));
+      symbol.setBounds(
+          View.GRID_WIDTH / 2 + (View.GRID_WIDTH * i + View.GRID_WIDTH / 5 * i),
+          View.GRID_HEIGHT / 2,
+          View.GRID_WIDTH,
+          View.GRID_HEIGHT);
+      symbolTable.add(symbol);
+    }
+    // starts memorize phase countdown
+    timer.countdown(Countdown.MEMORIZE_TIME);
+  }
+
+  private void castingPhase() {
+    if (!timer.isCounting()
+        || gojo.getSpells().size() == 5) { // if time's up or player has done inputting
+      // transit to attack phase, stop counting if there are time remaining (the moon is not red)
+      isCastingPhase = false;
+      timer.stopCounting();
+
+      checkInput();
+
+      attackPhaseSetup();
+    }
+  }
+
+  private void attackPhaseSetup() {
+    // hide input table and symbol table
+    inputPanel.setVisible(false);
+    symbolPanel.setVisible(false);
+    symbolTable.setVisible(false);
+    // starts attack phase countdown
+    timer.countdown(Countdown.RESULT_TIME);
+    updateScoreLabel();
+  }
+
+  private void checkInput() {
+    int correct = 0;
+
+    // check input
+    while (!gojo.getSpells().isEmpty())
+      if (gojo.getSpells().pop().equals(dragon.getWeakness().pop())) correct++;
+
+    // attack
+    switch (correct) {
+      case 0:
+        dragon.attack(gojo);
+        view.soundManager.playDragonAttack();
+        view.soundManager.playGojoHurt();
+        break;
+      case 5:
+        gojo.attack(correct, dragon);
+        view.soundManager.playGojoAttack();
+        break;
+      default:
+        gojo.attack(correct, dragon);
+        view.soundManager.playGojoAttack();
+        dragon.attack(gojo);
+        view.soundManager.playDragonAttack();
+        view.soundManager.playGojoHurt();
+    }
+
+    // update score
+    score += correct * 100;
+  }
+
+  private void updateScoreLabel() {
+    // Cập nhật hiển thị điểm số trên label
+    scoreLabel.setText("Score: " + score);
+  }
+
+  private void memorizePhase() {
+    if (isStartOfTheGame) {
+      isStartOfTheGame = false;
+      memorizePhaseSetup();
+    }
+    if (!timer.isCounting()) { // memorize phase ends, hide weakness table, starts input phase
+      // stop counting
+      timer.stopCounting();
+      // transit to casting phase
+      isMemorizePhase = false;
+      isCastingPhase = true;
+
+      castingPhaseSetup();
+    }
+  }
+
+  private void castingPhaseSetup() {
+    // show input table
+    inputPanel.setVisible(true);
+    // clear symbol table for player's input
+    symbolTable.removeAll();
+    // starts input phase countdown
+    timer.countdown(Countdown.INPUT_TIME);
+  }
+
+  public void castSpell(String symbolName) {
+    // add inputted symbol to table
+    spriteManager.setSymbolSpriteSize(View.GRID_WIDTH, View.GRID_HEIGHT);
+    JLabel symbol = new JLabel();
+    symbol.setIcon(spriteManager.getSymbolSprite(Integer.parseInt(symbolName.substring(6))));
+    symbol.setBounds(
+        View.GRID_WIDTH / 2
+            + (View.GRID_WIDTH * gojo.getSpells().size()
+                + View.GRID_WIDTH / 5 * gojo.getSpells().size()),
+        View.GRID_HEIGHT / 2,
+        View.GRID_WIDTH,
+        View.GRID_HEIGHT);
+    symbolTable.add(symbol);
+
+    gojo.castSpell(symbolName);
+  }
+
+  /** reset the game (when user start the game after pressing the back button) */
+  public void reset() {
+    isStartOfTheGame = true;
+    thread = new Thread(this);
+    gojo = new Gojo();
+    dragon = new Dragon();
+    timer = new Countdown();
+    isMemorizePhase = true;
+    isCastingPhase = false;
+    symbolPanel.setVisible(false);
+    inputPanel.setVisible(false);
+  }
+
+  public void start() {
+    thread.start();
+  }
+
+  /** exit game, stop the game thread */
+  public void stop() {
+    thread.interrupt();
+  }
+
+  @Override
+  public void run() {
+    while (!Thread.currentThread().isInterrupted()) {
+      long startTime = System.currentTimeMillis();
+      if (isMemorizePhase) memorizePhase();
+      else if (isCastingPhase) castingPhase();
+      else attackPhase();
+
+      int rand = (int) (Math.random() * 10000);
+      if (rand == 69) view.soundManager.playGojoIdle();
+      else if (rand == 420) view.soundManager.playDragonIdle();
+
+      long endTime = System.currentTimeMillis();
+
+      long sleepTime = 1000 / fps - (endTime - startTime);
+      try {
+        Thread.sleep(sleepTime > 0 ? sleepTime : 0);
+      } catch (InterruptedException e) {
+        return;
+      }
+      repaint();
+    }
+  }
+
+  public int getScore() {
+    return score;
+  }
+}
---
+++ src/view/View.java
@@ -0,0 +1,296 @@
+package view;
+
+import java.awt.*;
+import java.io.InputStream;
+import java.util.List;
+import javax.swing.*;
+import module.ScoreManager;
+import sound.SoundManager;
+
+public class View extends JFrame {
+
+  public static final int MAX_GRID = 16; // 16x16
+  private static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+  public static final int GRID_WIDTH = screenSize.width / MAX_GRID;
+  public static final int GRID_HEIGHT = screenSize.height / MAX_GRID;
+  
+  private final CardLayout cardLayout = new CardLayout();
+  public MouseController mouseController = new MouseController(this);
+  public GamePanel gamePanel;
+  public SoundManager soundManager = new SoundManager();
+  public Font gameFont;
+  private JPanel mainMenu;
+  private JPanel endScreen;
+  private JPanel highScoreScreen;
+  private JLabel scoresLabel; // label showing the high scores
+
+  private ScoreManager scoreManager = new ScoreManager();
+
+  public View() {
+    init();
+  }
+
+  private void init() {
+    setTitle("Memory Matrix Showdown");
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setLayout(cardLayout);
+    this.setResizable(false);
+
+    loadFont();
+
+    this.setLayout(cardLayout);
+    mainMenuInit();
+    gamePanelInit();
+    endScreenInit();
+    helpScreenInit();
+    highScoreScreenInit();
+
+    soundManager.playBackground();
+
+    //    this.pack();
+    this.setUndecorated(true);
+    this.setSize(screenSize);
+    this.setLocationRelativeTo(null);
+    this.setVisible(true);
+  }
+
+  private void loadFont() {
+    try {
+      InputStream is =
+          getClass().getClassLoader().getResourceAsStream("resource/fonts/game_font.ttf");
+      gameFont = Font.createFont(Font.TRUETYPE_FONT, is);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void mainMenuInit() {
+    mainMenu = new JPanel(null);
+
+    JLabel background = new JLabel();
+    background.setLayout(null);
+    ImageIcon icon =
+        new ImageIcon(getClass().getClassLoader().getResource("resource/images/background.png"));
+    Image img =
+        icon.getImage()
+            .getScaledInstance(GRID_WIDTH * MAX_GRID, GRID_HEIGHT * MAX_GRID, Image.SCALE_SMOOTH);
+    background.setIcon(new ImageIcon(img));
+    background.setBounds(0, 0, GRID_WIDTH * MAX_GRID, GRID_HEIGHT * MAX_GRID);
+    mainMenu.add(background);
+
+    int buttonWidth = 2 * GRID_WIDTH;
+    int buttonHeight = 2 * GRID_HEIGHT;
+
+    JLabel title = new JLabel();
+    icon = new ImageIcon(getClass().getClassLoader().getResource("resource/images/title.png"));
+    img = icon.getImage().getScaledInstance(7 * GRID_WIDTH, 4 * GRID_HEIGHT, Image.SCALE_SMOOTH);
+    title.setIcon(new ImageIcon(img));
+    title.setBounds(5 * GRID_WIDTH, GRID_HEIGHT, 7 * GRID_WIDTH, GRID_HEIGHT * 4);
+    background.add(title);
+
+    addButton(
+        background,
+        "start",
+        "resource/images/start_button.png",
+        5 * GRID_WIDTH,
+        6 * GRID_HEIGHT,
+        buttonWidth,
+        buttonHeight);
+    addButton(
+        background,
+        "highScore",
+        "resource/images/high_score_button.png",
+        5 * GRID_WIDTH,
+        12 * GRID_HEIGHT,
+        buttonWidth,
+        buttonHeight);
+    addButton(
+        background,
+        "help",
+        "resource/images/info_button.png",
+        10 * GRID_WIDTH,
+        12 * GRID_HEIGHT,
+        buttonWidth,
+        buttonHeight);
+    addButton(
+        background,
+        "exit",
+        "resource/images/exit_button.png",
+        10 * GRID_WIDTH,
+        6 * GRID_HEIGHT,
+        buttonWidth,
+        buttonHeight);
+
+    this.add(mainMenu, "mainMenu");
+  }
+
+  private void addButton(
+      JLabel container, String name, String imagePath, int x, int y, int width, int height) {
+    JLabel button = new JLabel();
+    button.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+    button.setName(name);
+    ImageIcon buttonIcon = new ImageIcon(getClass().getClassLoader().getResource(imagePath));
+    buttonIcon =
+        new ImageIcon(buttonIcon.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH));
+    button.setIcon(buttonIcon);
+    button.setBounds(x, y, width, height);
+    button.addMouseListener(mouseController);
+    container.add(button);
+  }
+
+  private void gamePanelInit() {
+    gamePanel = new GamePanel(this);
+    this.add(gamePanel, "gamePanel");
+  }
+
+  private void endScreenInit() {
+    endScreen = new JPanel(new BorderLayout());
+    endScreen.setName("endScreen");
+    endScreen.setBackground(Color.BLACK);
+    endScreen.setName("endScreen");
+    JLabel image = new JLabel();
+    image.setName("endScreen");
+    image.addMouseListener(mouseController);
+    ImageIcon icon =
+        new ImageIcon(getClass().getClassLoader().getResource("resource/images/game_over.png"));
+    icon =
+        new ImageIcon(
+            icon.getImage()
+                .getScaledInstance(
+                    GRID_WIDTH * MAX_GRID, GRID_HEIGHT * MAX_GRID, Image.SCALE_SMOOTH));
+    image.setIcon(icon);
+    endScreen.add(image, BorderLayout.CENTER);
+    JLabel text =
+        new JLabel(
+            "Final score: " + gamePanel.getScore(),
+            SwingConstants
+                .CENTER); // for some reason setting the text here make the frame pack in the proper
+    // size
+    text.setName("endScreen");
+    text.addMouseListener(mouseController);
+    text.setFont(gameFont.deriveFont(Font.BOLD, 80));
+    text.setForeground(Color.WHITE);
+    endScreen.add(text, BorderLayout.SOUTH);
+    this.add(endScreen, "endScreen");
+  }
+
+  private void helpScreenInit() {
+    JPanel helpScreen = new JPanel(null);
+    helpScreen.setName("helpScreen");
+
+    JLabel background = new JLabel();
+    background.setLayout(null);
+    background.setIcon(
+        new ImageIcon(getClass().getClassLoader().getResource("resource/images/background.png")));
+    background.setBounds(0, 0, GRID_WIDTH * MAX_GRID, GRID_HEIGHT * MAX_GRID);
+    helpScreen.add(background);
+
+    JLabel backButton = new JLabel();
+    backButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+    backButton.setName("back");
+    ImageIcon backIcon =
+        new ImageIcon(getClass().getClassLoader().getResource("resource/images/exit_button.png"));
+    backIcon =
+        new ImageIcon(
+            backIcon.getImage().getScaledInstance(GRID_WIDTH, GRID_HEIGHT, Image.SCALE_SMOOTH));
+    backButton.setIcon(backIcon);
+    backButton.setBounds(10 * GRID_WIDTH + 80, 3 * GRID_HEIGHT - 30, GRID_WIDTH, GRID_HEIGHT);
+    backButton.addMouseListener(mouseController);
+    background.add(backButton);
+
+    JLabel helpBoard = new JLabel();
+    helpBoard.setLayout(null);
+    ImageIcon icon =
+        new ImageIcon(getClass().getClassLoader().getResource("resource/images/help_board.png"));
+    Image img =
+        icon.getImage().getScaledInstance(8 * GRID_WIDTH, 11 * GRID_HEIGHT, Image.SCALE_SMOOTH);
+    helpBoard.setIcon(new ImageIcon(img));
+    helpBoard.setBounds(4 * GRID_WIDTH, 2 * GRID_HEIGHT, 8 * GRID_WIDTH, 11 * GRID_HEIGHT);
+    background.add(helpBoard);
+
+    this.add(helpScreen, "helpScreen");
+  }
+
+  private void highScoreScreenInit() {
+    highScoreScreen = new JPanel(new BorderLayout());
+    highScoreScreen.setPreferredSize(new Dimension(GRID_WIDTH * MAX_GRID, GRID_HEIGHT * MAX_GRID));
+    JLabel background = new JLabel();
+    background.setLayout(null);
+
+    JLabel backButton = new JLabel();
+    backButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+    backButton.setName("back");
+    ImageIcon backIcon =
+        new ImageIcon(getClass().getClassLoader().getResource("resource/images/exit_button.png"));
+    backIcon =
+        new ImageIcon(
+            backIcon.getImage().getScaledInstance(GRID_WIDTH, GRID_HEIGHT, Image.SCALE_SMOOTH));
+    backButton.setIcon(backIcon);
+    backButton.setBounds(9 * GRID_WIDTH + 90, 2 * GRID_HEIGHT + 40, GRID_WIDTH, GRID_HEIGHT);
+    backButton.addMouseListener(mouseController);
+    background.add(backButton);
+
+    background.setIcon(
+        new ImageIcon(getClass().getClassLoader().getResource("resource/images/background.png")));
+    highScoreScreen.add(background, BorderLayout.CENTER);
+
+    JLabel image = new JLabel();
+    image.setLayout(null);
+    ImageIcon icon =
+        new ImageIcon(getClass().getClassLoader().getResource("resource/images/table.png"));
+    icon =
+        new ImageIcon(
+            icon.getImage()
+                .getScaledInstance(6 * GRID_WIDTH, GRID_HEIGHT * 10, Image.SCALE_SMOOTH));
+    image.setIcon(icon);
+    image.setBounds(5 * GRID_WIDTH, 2 * GRID_HEIGHT, GRID_WIDTH * 6, GRID_HEIGHT * 10);
+    background.add(image);
+
+    JLabel titleText = new JLabel("High Scores", SwingConstants.CENTER);
+    titleText.setFont(gameFont.deriveFont(Font.BOLD, 60));
+    titleText.setBounds(GRID_WIDTH + 30, GRID_HEIGHT, GRID_WIDTH * 3, GRID_HEIGHT);
+    image.add(titleText);
+
+    scoresLabel = new JLabel();
+    scoresLabel.setFont(gameFont.deriveFont(Font.PLAIN, 50));
+    scoresLabel.setBounds(2 * GRID_WIDTH + 30, GRID_HEIGHT, 4 * GRID_WIDTH, 9 * GRID_HEIGHT);
+    updateHighScoreScreen();
+    image.add(scoresLabel, BorderLayout.CENTER);
+
+    this.add(highScoreScreen, "highScore");
+  }
+
+  private void updateHighScoreScreen() {
+    List<Integer> scores = scoreManager.getScores();
+    String scoresText =
+        String.format(
+            "<html>"
+                + "<ol>"
+                + "<li> %d </li>"
+                + "<li> %d </li>"
+                + "<li> %d </li>"
+                + "<li> %d </li>"
+                + "<li> %d </li>"
+                + "</ol>"
+                + "</html>",
+            scores.get(0), scores.get(1), scores.get(2), scores.get(3), scores.get(4));
+    scoresLabel.setText(scoresText);
+  }
+
+  public void updateScore() {
+    JLabel text = (JLabel) endScreen.getComponent(1); // 0 is the image label, 1 is the text label
+    text.setText("Final score: " + gamePanel.getScore());
+    scoreManager.addScore(gamePanel.getScore());
+    updateHighScoreScreen();
+  }
+
+  public void changePanel(String panelName) {
+    if (panelName.equals("gamePanel")) gamePanel.reset();
+
+    cardLayout.show(this.getContentPane(), panelName);
+  }
+
+  public void exit() {
+    System.exit(0);
+  }
+}
