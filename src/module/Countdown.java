af0f5fc28271732260308822313962fe4930b0a3
duongk67 <151826938+duongk67@users.noreply.github.com>
2023-12-17 17:48:57 +0700

    Update Countdown.java

--- src/module/Countdown.java
+++ src/module/Countdown.java
@@ -1,35 +1,42 @@
-public class Countdown implements Runnable{
-    private int time;
-    private Thread thread;
-    public static final int inputTime = 9;
-    public static final int resultTime = 1;
-    public static final int memorizeTime = 5;
-    public int getTime(){
-        return time;
-    }
+package module;
 
-    public Countdown(int time) {
-        thread = new Thread(this);
-        this.time = time;
-        thread.start();
+public class Countdown implements Runnable {
+  public static final int MEMORIZE_TIME = 9;
+  public static final int INPUT_TIME = 9;
+  public static final int RESULT_TIME = 1;
+  private int time;
+  private Thread thread;
+
+  public int getTime() {
+    return time;
+  }
+
+
+  public void countdown(int time) {
+    thread = new Thread(this);
+    this.time = time;
+    thread.start();
+  }
+
+  public boolean isCounting() {
+    return time > 0;
+  }
+
+  public void stopCounting() {
+    time = -1;
+    thread.interrupt();
+  }
+
+  @Override
+  public void run() {
+    while (time > 0) {
+      try {
+        Thread.sleep(1000);
+      } catch (InterruptedException e) {
+        return; // thread is interrupted, stop counting, stop thread
+      }
+      time--;
     }
-    public boolean isCounting(){
-        return time > 0;
-    }
-    public void stopCounting(){
-        time = -1;
-        thread.interrupt();
-    }
-    @Override
-    public void run() {
-        while(time > 0){
-            try{
-                Thread.sleep(1000);
-            }catch(InterruptedException e) {
-                return; // thread is interrupted, stop counting, stop thread
-            }
-            time--;
-        }
-        time--;
-    }
+    time--;
+  }
 }
