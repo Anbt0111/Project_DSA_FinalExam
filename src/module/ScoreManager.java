1216ac6f08bc856c3186a53bdf23ac970f7e0c6d
duongk67 <151826938+duongk67@users.noreply.github.com>
2023-12-17 17:49:19 +0700

    Update ScoreManager.java

--- src/module/ScoreManager.java
+++ src/module/ScoreManager.java
@@ -1,77 +1,82 @@
+package module;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
 public class ScoreManager {
-    private final String WORKING_DIRECTORY = System.getProperty("user.dir");
-    private final String HIGH_SCORE_FILE_PATH = WORKING_DIRECTORY + "/highscore.txt";
-    private List<Integer> scores;
-    private File highScoreFile;
-    private Scanner in;
-    private FileWriter out;
-    public ScoreManager(){
-        scores = new ArrayList<>(6);
-        highScoreFile = new File(HIGH_SCORE_FILE_PATH);
-        try{
-            if(!highScoreFile.exists()) {
-                highScoreFile.createNewFile();
-            }
-            in = new Scanner(highScoreFile);
-            while(in.hasNextInt()){
-                scores.add(in.nextInt());
-            }
-            in.close();
-            while (scores.size() < 5) {
-                scores.add(0);// if there are not enough scores, add 0s
-            }
-        }catch(IOException e){
-            e.printStackTrace();
-        }
+  private final String WORKING_DIRECTORY = System.getProperty("user.dir");
+  private final String HIGH_SCORE_FILE_PATH = WORKING_DIRECTORY + "/highscore.txt";
+  private List<Integer> scores;
+  private File highScoreFile;
+  private Scanner in;
+  private FileWriter out;
+
+  public ScoreManager() {
+    scores = new ArrayList<>(6);
+    highScoreFile = new File(HIGH_SCORE_FILE_PATH);
+    try {
+      if (!highScoreFile.exists()) highScoreFile.createNewFile();
+      in = new Scanner(highScoreFile);
+
+      while (in.hasNextInt()) scores.add(in.nextInt());
+      in.close();
+
+      // if there are not enough scores, add 0s
+      while (scores.size() < 5) scores.add(0);
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-    /**
-     * get scores by reading high score file
-     * @return list of scores
-     */
-    public List<Integer> getScores(){
-        in = new Scanner(HIGH_SCORE_FILE_PATH);
-        for (int i = 0; in.hasNextInt(); i++) {
-            scores.set(i, in.nextInt());
-        }
-        in.close();
-        return scores;
+  }
+
+  /**
+   * get scores by reading high score file
+   * @return list of scores
+   */
+  public List<Integer> getScores() {
+    in = new Scanner(HIGH_SCORE_FILE_PATH);
+    for (int i = 0; in.hasNextInt(); i++) scores.set(i, in.nextInt());
+    in.close();
+    return scores;
+  }
+
+  /**
+   * add the final scores to the high score file after a game ends
+   * @param score
+   */
+  public void addScore(int score) {
+    scores.add(score);
+    sort();
+    updateHighScoreFile();
+  }
+
+  private void updateHighScoreFile() {
+    // overwrite the file
+    try {
+      out = new FileWriter(highScoreFile);
+      for (int score : scores) out.write(score + " ");
+      out.close();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-    public void addScore(int score){
-        scores.add(score);
-        sort();
-        updateHighScoreFile();
+  }
+
+  // pretty redundant, but gotta implement some algorithm somehow
+  private void sort() {
+    // insertion sort
+    for (int i = 1; i < scores.size(); i++) {
+      int key = scores.get(i);
+      int j = i - 1;
+      while (j >= 0 && scores.get(j) < key) {
+        scores.set(j + 1, scores.get(j));
+        j--;
+      }
+      scores.set(j + 1, key);
     }
-    // pretty redundant, but gotta implement some algorithm somehow
-    private void updateHighScoreFile(){
-        // overwrite the file
-        try{
-            out = new FileWriter(highScoreFile);
-            for (int score : scores) {
-                out.write(score + " ");
-            }
-            out.close();
-        }catch(IOException e){
-            e.printStackTrace();
-        }
-    }
-    private void sort() {
-        // insertion sort
-        for (int i = 1; i < scores.size(); i++) {
-            int key = scores.get(i);
-            int j = i - 1;
-            while (j >= 0 && scores.get(j) < key) {
-                scores.set(j + 1, scores.get(j));
-                j--;
-            }
-            scores.set(j + 1, key);
-        }
-        // also delete the 6th score as it is out of the high score board, no need to keep track of it
-        if (scores.size() > 5) scores.remove(5);
-    }
+    // also delete the 6th score as it is out of the high score board, no need to keep track of it
+    if (scores.size() > 5) scores.remove(5);
+  }
 }
