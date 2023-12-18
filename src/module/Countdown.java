61836ce3dd24ecde76e7d6520b05773ac5969680
TruongQuocAnh76 <truongquocanh0706@gmail.com>
2023-12-18 22:48:07 +0700

    fix bug when using back button throws thread is null exception

--- src/module/Countdown.java
+++ src/module/Countdown.java
@@ -1,42 +1,44 @@
 package module;
 
 public class Countdown implements Runnable {
-  public static final int MIN_MEMORIZE_TIME = 3;
-  public static final int MIN_INPUT_TIME = 5;
-  public static final int RESULT_TIME = 1;
-  private int time;
-  private Thread thread;
+    public static final int MIN_MEMORIZE_TIME = 3;
+    public static final int MIN_INPUT_TIME = 5;
+    public static final int RESULT_TIME = 1;
+    private int time;
+    private Thread thread;
 
-  public int getTime() {
-    return time;
-  }
-
-
-  public void countdown(int time) {
-    thread = new Thread(this);
-    this.time = time;
-    thread.start();
-  }
-
-  public boolean isCounting() {
-    return time > 0;
-  }
-
-  public void stopCounting() {
-    time = -1;
-    thread.interrupt();
-  }
-
-  @Override
-  public void run() {
-    while (time > 0) {
-      try {
-        Thread.sleep(1000);
-      } catch (InterruptedException e) {
-        return; // thread is interrupted, stop counting, stop thread
-      }
-      time--;
+    public int getTime() {
+        return time;
     }
-    time--;
-  }
+
+
+    public void countdown(int time) {
+        thread = new Thread(this);
+        this.time = time;
+        thread.start();
+    }
+
+    public boolean isCounting() {
+        return time > 0;
+    }
+
+    public void stopCounting() {
+        if (thread != null) {
+            time = -1;
+            thread.interrupt();
+        }
+    }
+
+    @Override
+    public void run() {
+        while (time > 0) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                return; // thread is interrupted, stop counting, stop thread
+            }
+            time--;
+        }
+        time--;
+    }
 }
